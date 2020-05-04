58a0d057a30b897d5ce09bde38e3e861ea03c477
Paul Naidoo <p.naidoo.1@research.gla.ac.uk>
2020-05-04 15:55:31 +0100

    New script: One to (properly) generate and save histograms, one to fit them.

---
+++ scripts/cutstudy/cutstudyhistos.h
@@ -0,0 +1,11 @@
+#ifndef plothistos_h
+#define plothistos_h
+
+/*  Functions  */
+void cuthistos();
+
+/*  Histograms   */
+TH1F *ggIM_h[6];
+TH1F *ggIM_coneangle_h[5][6];
+TH1F *ggIM_mm2_h[3][6];
+#endif
