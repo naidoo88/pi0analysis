9470ebd603a3ecad15a332255876c23381e81e44
Paul Naidoo <p.naidoo.1@research.gla.ac.uk>
2020-05-20 20:12:25 +0100

    added fits

--- scripts/plotasym.cpp
+++ scripts/plotasym.cpp
@@ -20,10 +20,13 @@
 #include <TLorentzVector.h>
 #include <TH1.h>
 #include <TH2.h>
+#include <TF1.h>
 #include <TChain.h>
 #include <TCanvas.h>
 #include <TBenchmark.h>
 #include <TString.h>
+#include <TMath.h>
+#include <TFile.h>
 
 #include "clas12reader.h"
 
@@ -45,6 +48,8 @@ void plotasym(TString datafile, TString outfile){
     bool   flag_cuts_broadcone;
     bool   flag_cuts_spectMP;
     double MM2_total;
+    double cop_Nvg_vgnew;  
+
 
 	int n_zerohel = 0;
 
@@ -75,6 +80,7 @@ void plotasym(TString datafile, TString outfile){
     chain.SetBranchAddress("flag_cuts_broadcone", &flag_cuts_broadcone);
     chain.SetBranchAddress("flag_cuts_spectMP",   &flag_cuts_spectMP);  
     chain.SetBranchAddress("MM2_total",           &MM2_total);
+    chain.SetBranchAddress("cop_Nvg_vgnew",       &cop_Nvg_vgnew);  
 
 
 	for(Int_t i=0;i<chain.GetEntries();i++){
@@ -86,7 +92,8 @@ void plotasym(TString datafile, TString outfile){
         if(flag_cuts_broadcone != 1) continue; //} correspond to very loose excl cuts  
         if(flag_cuts_spectMP   != 1) continue; //}
         if(!(MM2_total > -0.05155 && MM2_total < 0.03455)) continue; //can be changed to flag_cuts_3sigMM2 soon
-    
+        if(!(std::abs(cop_Nvg_vgnew) < 5)) continue; //coplanarity between Recoil/virtPhot & newPart/virtPhot
+
         R_N_phi_h  ->Fill(phi_Nnew);
         R_vg_phi_h ->Fill(phi_Nvg);
         N_vg_phi_h ->Fill(phi_vgnew);
@@ -115,6 +122,17 @@ void plotasym(TString datafile, TString outfile){
 
 	N_vg_helasym_h = (TH1F*) N_vg_helplus_h->GetAsymmetry(N_vg_helmin_h);
     N_vg_helasym_h->SetName("Asym_N_vg_h");
+
+    //Fit the asymmetries
+    TString fit_fxn = "[0] + [1]*sin(x)";
+    TF1 *asymm_fits[3];
+    asymm_fits[0] = new TF1("Asym_R_vg_fit", fit_fxn, 0, 360);
+    asymm_fits[1] = new TF1("Asym_R_N_fit",  fit_fxn, 0, 360);
+    asymm_fits[2] = new TF1("Asym_N_vg_fit", fit_fxn, 0, 360);
+
+    R_N_helasym_h  -> Fit(asymm_fits[0], "RM");
+    R_vg_helasym_h -> Fit(asymm_fits[1], "RM");
+    N_vg_helasym_h -> Fit(asymm_fits[2], "RM");
 
     //Write out histograms
     TFile *Out_File = new TFile(outfile, "recreate"); 
