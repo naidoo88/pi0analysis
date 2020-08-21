4ed355041669be6ac360ad6f9ee494a5821587e8
Paul Naidoo <p.naidoo.1@research.gla.ac.uk>
2020-08-21 15:55:24 +0100

    Added buffer-dir creation and post-merge cleanup

--- runAnaProof.C
+++ runAnaProof.C
@@ -9,6 +9,8 @@
 #include "TProof.h"
 #include "HipoChain.h"
 #include "TSystem.h"
+#include "TBenchmark.h"
+#include "TString.h"
 #include "pi0selector.h"
 
 using std::cout;
@@ -16,12 +18,37 @@ using std::endl;
 
 void runAnaProof(const Char_t input_list[], const Char_t outfilename[]){
 
+  TStopwatch time;
+  time.Start();
+
   std::ifstream list_of_files;
   char file_name[200];
   char last_file[200];
 
   int filecount = 0;
   TString outbuffname; //outfile buffer to be combined with hadd at the end of the script.
+  
+  //retrieve path of output file for buffer path
+  TString dirname_cmd = Form("dirname %s", outfilename);
+  TString outpath = gSystem->GetFromPipe(dirname_cmd.Data());
+  TString buffpath = Form("%s/.outbuff/", outpath.Data());
+  cout << endl << ">>> Buffer directory path is: " << buffpath << endl;
+
+  //create buffer directory if it doesn't exist
+  if((gSystem->AccessPathName(buffpath.Data()))){ //NB weird return convention: 0 => exists.
+    cout << ">>> Buffer directory " << buffpath << " does not exist. Creating...";
+    if (gSystem->MakeDirectory(buffpath.Data()) != 0){
+      cout << endl << endl << " FAILED!!! Check disk space or permissions?" << endl << ">>> Process will now terminate." << endl;
+      return;
+    }
+    else cout << " SUCCESS!" << endl << endl;
+  }
+  else{ //remove any previous contents if exists
+    cout << ">>> Directory '" << buffpath << "' exists.  Removing any previous contents." << endl << endl;
+    TString rmprev_cmd = Form("rm %s*", buffpath.Data());
+    cout << rmprev_cmd << 
+    gSystem->Exec(rmprev_cmd);
+  }
 
   list_of_files.open(input_list);
   if (list_of_files.is_open()){
@@ -44,21 +71,30 @@ void runAnaProof(const Char_t input_list[], const Char_t outfilename[]){
 
         clas12root::pi0selector selector(&chain);
 
-        //TString filecount_str = filecount;
-        outbuffname.Form("buff%d.root", filecount);
-        cout << outbuffname << endl;
-
+        outbuffname.Form("%sbuff%d.root", buffpath.Data(), filecount);
+        
         selector.SetOutFileName(outbuffname);
 
         gProof->Process(&selector,chain.GetNRecords());
 
         filecount++;
-      }
-    }
-  }
+      }// duplicate check
+      sprintf(last_file, "%s", file_name); // update "last_file"    
+    }// filelist loop
+  }// list-open check
 
-  TString hadd_cmd;
-  hadd_cmd.Form("hadd %s .outbuff/*", outfilename);
+  cout << endl << ">>> Merging buffer files into " << outfilename << endl;
+  TString hadd_cmd = Form("hadd -f %s %s*", outfilename, buffpath.Data());
+  cout << hadd_cmd << endl;
   gSystem->Exec(hadd_cmd.Data());
+
+  cout << endl << ">>> Cleaning up buffer output files... " << endl;
+  TString cleanup_cmd = Form("rm -rf %s", buffpath.Data());
+  cout << cleanup_cmd << endl;
+  gSystem->Exec(cleanup_cmd);
+  //############# FINAL CLEAN UP 
+
+  time.Stop();
+  time.Print();
 
 }
